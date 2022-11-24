f31c9f1f8cc9f6849cbb98e9c581cee25eef3893
Wojciech Malarski <w.malarski@gmail.com>
2022-11-24 20:52:15 +0100

    updates env parsing

--- src/server/serverEnv.ts
+++ src/server/serverEnv.ts
@@ -5,4 +5,11 @@ const schema = z.object({
   VITE_SUPABASE_URL: z.string().url(),
 });
 
-export const serverEnv = schema.parse(import.meta.env);
+const nodeEnv = process.env;
+const viteEnv = import.meta.env;
+
+export const serverEnv = schema.parse({
+  VITE_SUPABASE_ANON_KEY:
+    nodeEnv.VITE_SUPABASE_ANON_KEY || viteEnv.VITE_SUPABASE_ANON_KEY,
+  VITE_SUPABASE_URL: nodeEnv.VITE_SUPABASE_URL || viteEnv.VITE_SUPABASE_URL,
+});
