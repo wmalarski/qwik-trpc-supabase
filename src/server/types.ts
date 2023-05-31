5a2d35c39925f65bdaf3801fb644da6f6d169bac
Wojciech Malarski <w.malarski@gmail.com>
2023-05-31 22:38:37 +0200

    packages bumped

--- src/server/types.ts
+++
@@ -1,7 +0,0 @@
-import type { RequestEvent, RequestEventLoader } from "@builder.io/qwik-city";
-import type { RequestEventAction } from "@builder.io/qwik-city/middleware/request-handler";
-
-export type ServerEvent =
-  | RequestEventLoader
-  | RequestEvent
-  | RequestEventAction;
